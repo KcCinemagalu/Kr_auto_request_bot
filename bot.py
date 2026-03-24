6fb8816c11946dd4bdf7f54cd2b93503247f5e06
KcCinemagalu <124599828+KcCinemagalu@users.noreply.github.com>
2026-03-24 16:20:05 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,8 +6,8 @@ from os import environ as env
 import asyncio, datetime, time
 
 
-ACCEPTED_TEXT = "Hey {user}\n\nYour Request For {chat} Is Accepted ✅\n\n𝗝𝗢𝗜𝗡: @Kr_CineHub For ಕನ್ನಡ ಹೊಸ ಸಿನಿಮಾ \n\n📥 Send 👉 /start To Get 𝗞𝗮𝗻𝗻𝗮𝗱𝗮 𝗥𝗼𝗰𝗸𝗲𝗿𝘀 𝗖𝗶𝗻𝗲𝗛𝘂𝗯.\n\nJoin👇👇\n{joinlink}"
-START_TEXT = "Hai {}\n\nI am Auto Request Accept Bot With Working For All Channel. Add Me In Your Channel To Use \n\nJoin👇👇\n{}"
+ACCEPTED_TEXT = "Hey {user}\n\nYour Request For {chat} Is Accepted ✅\n\n𝗝𝗢𝗜𝗡: @Kr_CineHub For ಕನ್ನಡ ಹೊಸ ಸಿನಿಮಾ \n\n📥 𝗦𝗲𝗻𝗱 👉 /start 𝗧𝗼 𝗚𝗲𝘁 𝗞𝗮𝗻𝗻𝗮𝗱𝗮 𝗥𝗼𝗰𝗸𝗲𝗿𝘀 𝗖𝗶𝗻𝗲𝗛𝘂𝗯.\n\n𝗝𝗢𝗜𝗡👇👇\n{joinlink}"
+START_TEXT = "Hai {}\n\nI am Auto Request Accept Bot With Working For All Channel. Add Me In Your Channel To Use \n\n𝗝𝗢𝗜𝗡👇👇\n{}"
 
 API_ID = int(env.get('API_ID'))
 API_HASH = env.get('API_HASH')
